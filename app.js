91291e353f127bd7da4ff6b9fe437fee516ad9ce
00010132 <nasibullo.me@mail.ru>
2021-06-29 23:33:37 +0500

    READ + Create done

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.get('/', (req, res) => {
         if (error) throw error;
 
         const posts = JSON.parse(data);
-        console.log(posts);
         res.render('home', {posts: posts});
     });
 });
@@ -22,10 +21,51 @@ app.get('/create', (req, res) => {
     res.render('create');
 });
 
+app.post('/create', (req, res) => {
+    const {title, description, location} = req.body;
+
+    fs.readFile('./data/posts.json', (err, data) => {
+        if (err) throw err;
+
+        const posts = JSON.parse(data);
+
+        posts.push({
+            id: id(),
+            title,
+            description,
+            location,
+            published_at: new Date()
+        });
+
+        fs.writeFile('./data/posts.json', JSON.stringify(posts), err => {
+            if (err) throw err;
+
+            res.render('home', {posts: posts})
+        })
+    });
+});
+
+app.get('/posts/view/:id', (req, res) => {
+    const id = req.params.id;
+
+    fs.readFile('./data/posts.json', (error, data) => {
+        if (error) throw error;
+
+        const posts = JSON.parse(data);
+        const post = posts.filter(post => post.id === id)[0] ;
+        res.render('view', {post});
+    });
+
+})
+
 app.listen(8000, err => {
     if (err) {
         console.log(err);
     }
 
     console.log('Server is running on port 8000...');
-})
+});
+
+function id() {
+    return '_' + Math.random().toString(36).substr(2,9);
+}
